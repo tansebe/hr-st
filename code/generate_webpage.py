2663c105d84cf4af82f72d10e6c40b7c5502fcbb
Christian Casey <christiancasey86@gmail.com>
2022-02-28 16:54:46 +0100

    Commit before rolling back to get page one glyphs back

--- code/generate_webpage.py
+++ code/generate_webpage.py
@@ -1,8 +1,13 @@
 
+import glob
+
+from PIL import Image
+
 HTML_HEADER = """<!DOCTYPE html>
 <html>
   <head>
     <title>hr-st</title>
+    <link rel="stylesheet" href="index.css" />
   </head>
   <body>
     """
@@ -11,41 +16,47 @@ HTML_FOOTER = """
 </html>
 """
 
+
+# Our main bucket to be filled
 html = ''
 
-import glob
-pageList = glob.glob("../mdc_pages/page*.gly")
+# Use the number of MdC pages to decide the total number of pages for the webpage
+# Note that this may not be correct, but any errors 
+# (say, an empty MdC file that adds one to this number)
+# have only minor consquences
+pageList = sorted(glob.glob("../mdc_pages/page*.gly"))
 numPages = len(pageList)
 
+# We don't assume that the numbers in the filenames are valid
+# We simply make the appropriate spaces and fix errors after
 for i in range(numPages):
+    currentPage = i+1 # Let's just make life easier whenever we can
     
-    html += '<h1>Manuscript Page %i</h1>' % i
+    html += '\n\n    <h2>Manuscript Page %i</h2>\n' % currentPage
     
-# for pageName in pageList:
-#     pageNumber = int(pageName[-6:-4])
-#     print('Spliting lines in page number %i' % pageNumber)
-#     f = open(pageName, 'r')
-#     page = f.read()
-#     f.close()
-# 
-#     lines = page.split('\n')
-# 
-#     lineNumber = 1
-#     for line in lines:
-# 
-#         if len(line) < 2:
-#             continue
-# 
-#         if line[0:2] == "++":
-#             # print('header line')
-#             continue
-# 
-#         newFilename = '%spage%02i_line%03i.txt' % (OUTPUT_DIR, pageNumber, lineNumber)
-#         f = open(newFilename, 'w')
-#         f.write(JSESH_HEADER + '\n' + line)
-#         f.close()
-# 
-#         lineNumber += 1
+    # Use the number of hieratic images to decide the number of lines for the page
+    lineList = sorted(glob.glob('../hieratic_lines/page%02i/*.png' % currentPage))
+    numLines = len(lineList)
+    
+    for j in range(numLines):
+        currentLine = j+1 # Easy life
+        
+        
+        im = Image.open('../hieratic_lines/page%02i/page%02i_line%03i.png' % (currentPage, currentPage, currentLine))
+        width = im.size[0]
+        
+        html += '\n\n'
+        html += '\n    <div class="line">'
+        html += '\n    <h3>Line %i</h3>' % currentLine
+        html += '\n      <div class="hieratic">'
+        html += '\n        <img src="../hieratic_lines/page%02i/page%02i_line%03i.png" />' % (currentPage, currentPage, currentLine)
+        html += '\n      </div>'
+        html += '\n      <div class="hieroglyphic">'
+        html += '\n        <img src="../png_lines/page%02i_line%03i.png" width="%ipx" class="hieroglyphic" />' % (currentPage, currentLine, width)
+        html += '\n      </div>'
+        html += '\n    </div>'
+
+
 
 html = HTML_HEADER + html + HTML_FOOTER
 f = open('../docs/hr-st.html', 'w')
